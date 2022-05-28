35d86ba312c01ec10eff0643634d4a4e8c880bd1
RezovV <63051246+RezovV@users.noreply.github.com>
2022-05-28 21:21:55 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Ucitaj from './routes/Ucitaj';
+import Dodaj from './routes/Dodaj';
+import Nav from './routes/Navigacija';
+import Edit from './routes/Edit';
+import FilterTable from './routes/FilterTable';
+
+
 
 function App() {
   return (
-    <>
-    </>
+   <BrowserRouter>
+   <Nav />
+      <Routes>
+        <Route path='/Ucitaj' element={<Ucitaj/>} />
+        <Route path='/Dodaj'element={<Dodaj/>} />
+        <Route path='/Ucitaj/Edit/:EditID' element={<Edit />} />
+        <Route path='/Pretrazivanje' element={<FilterTable />} />
+      </Routes>
+   </BrowserRouter>
   );
 }
 
